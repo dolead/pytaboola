a17daa27fe5da7e0fb8340d309c1225edb1dad6c
Antoine Français <antoine.francais@dolead.com>
2018-06-04 14:04:01 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pytaboola',
-    version='0.1.0',
+    version='0.1.1',
     packages=['pytaboola', 'pytaboola.utils', 'pytaboola.services'],
     url='https://github.com/dolead/pytaboola',
     keywords='taboola api',
