d247f07f9e51045b4a32f914a4408e228ca4772b
Antoine Français <antoine.francais@dolead.com>
2018-06-04 12:28:27 +0200

    Prout

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pytaboola',
-    version='0.1.0',
+    version='0.1.2',
     packages=['pytaboola', 'pytaboola.utils', 'pytaboola.services'],
     url='https://github.com/dolead/pytaboola',
     keywords='taboola api',
     license='MIT',
-    author='Antoine Français',
+    author='Antoine Francais',
     author_email='antoine.francais@gmail.com',
     maintainer="Dolead",
     maintainer_email="it@dolead.com",
     description='Python client for Taboola API',
+    long_description=long_description,
     classifiers=[
           "Programming Language :: Python :: 3",
           "Topic :: Software Development :: Libraries :: Python Modules",
